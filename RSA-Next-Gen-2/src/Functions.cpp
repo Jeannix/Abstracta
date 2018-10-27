094b57c9305117b6bfce3670c4cad704c047dd00
Jean C <jean_16jean@hotmail.com>
2018-10-27 01:54:04 -0500

    nex jen HD 28K falta rubrica y Firma con descifrado #cochino

---
+++ RSA-Next-Gen-2/src/Functions.cpp
@@ -0,0 +1,164 @@
+#include "Functions.h"
+
+ZZ modulo(ZZ a, ZZ n)
+{
+    ZZ q, r;
+	q = a / n;
+	r = a - (q*n);
+	if (r < 0)
+		r += n;
+	return r;
+}
+ZZ euclides(ZZ a, ZZ b)
+{
+     ZZ residuo = modulo(a,b);
+     while(residuo!=0)
+     {
+         a = b;
+         b = residuo;
+         residuo = modulo(a,b);
+     }
+     return b;
+}
+ZZ mcd(ZZ a, ZZ b)
+{
+    ZZ resultado;
+    resultado = 1;
+    ZZ i;
+    for(i=1;i<=a;i++)
+    {
+        if((modulo(a,i)==0)&&(modulo(b,i)==0))
+        {
+            resultado = i;
+        }
+    }
+    return resultado;
+}
+
+vector <ZZ> extendido(ZZ a, ZZ b)
+{
+    vector <ZZ> resultados;
+    ZZ r1,s1,t1,r2,s2,t2,q,r;
+    r1 = a;
+    r2 = b;
+    s1 = 1;
+    s2 = 0;
+    t1 = 0;
+    t2 = 1;
+    while(r2>0)
+    {
+        q = r1/r2;
+
+        ZZ r = r1 - q * r2;
+        r1 = r2;
+        r2 = r;
+
+        ZZ s = s1 - q * s2;
+        s1 = s2;
+        s2 = s;
+
+        ZZ t = t1 - q * t2;
+        t1 = t2;
+        t2 = t;
+
+    }
+    resultados.push_back(r1);
+    resultados.push_back(s1);
+    resultados.push_back(t1);
+    return resultados;
+}
+
+ZZ inversa(ZZ a, ZZ alfabeto)
+{
+    ZZ x = extendido(a,alfabeto)[1];
+    if(x<0)
+    {
+        return modulo(x,alfabeto);
+    }
+    return x;
+}
+
+ZZ potencia(ZZ a, ZZ b, ZZ modu)
+{
+    ZZ result = to_ZZ(1);
+    ZZ n = to_ZZ(2);
+    ZZ x = to_ZZ(a);
+    while(b != 0){
+        if(modulo(b,n)==1){
+            result = modulo((result * x),modu);
+        }
+        x = modulo((x * x),modu);
+        b = b/2;
+    }
+    return result;
+}
+string int_to_string(int s){
+    stringstream ss;
+    ss<<s;
+    string aux=ss.str();
+    return aux;
+}
+
+int string_to_int(string s){
+    stringstream geek(s);
+    int x = 0;
+    geek >> x;
+    return x;
+}
+//ZZ potencia(ZZ x, ZZ y)
+//{
+//    ZZ res = ZZ(1);
+//
+//    while (y > 0)
+//    {
+//        if (y & 1)
+//            res = res*x;
+//        y = y>>1;
+//        x = x*x;
+//    }
+//    return res;
+//}
+
+string zzToString(ZZ z) {
+    std::stringstream ss;
+    ss << z;
+    return ss.str();
+}
+
+ZZ stringTozz(string str){
+    ZZ number(INIT_VAL, str.c_str());
+    return number;
+}
+
+ZZ int_ZZ(int n){
+    ZZ z;
+    conv(z,n);
+    return z;
+}
+
+int ZZ_int(ZZ z){
+    int n;
+    conv(n,z);
+    return n;
+}
+
+ZZ phi_euler(ZZ p, ZZ q)
+{
+    return ((p-1)*(q-1));
+}
+string invertir(string b){
+    string temp;
+    for(int i=0;i<b.size();i++){
+        temp+=b[b.size()-1-i];
+    }
+    return temp;
+}
+string bin(ZZ a){
+    string tmp;
+    while(a!=ZZ(0)){
+        tmp+=zzToString(ZZ(a%2));
+        a/=ZZ(2);
+   }
+   tmp=invertir(tmp);
+   return tmp;
+}
---
+++ RSA-Next-Gen-2/src/RSA.cpp
@@ -0,0 +1,153 @@
+#include "RSA.h"
+
+RSA::RSA(){
+    long bits=8;
+    p=RandomBits_ZZ(bits);
+    while((ProbPrime(p))!=1){
+        p=RandomBits_ZZ(bits);
+    }
+    cout<<p<<endl;
+    q=RandomBits_ZZ(bits);
+    while((ProbPrime(q))!=1){
+        q=RandomBits_ZZ(bits);
+    }
+    cout<<q<<endl;
+    n=p*q;
+
+    cout <<"N : "<<n<<endl;
+    phi_n = phi_euler(p, q);
+    cout<<"Phi : "<<phi_n<<endl;
+
+    e = RandomBits_ZZ(bits);
+    while(euclides(e,phi_n)!=1){
+        e = RandomBits_ZZ(bits);
+    }
+
+    cout <<"E : "<<e<<endl;
+    d = inversa(e, phi_n);
+    cout <<"D : "<<d<<endl;
+    cout <<"Clave publica : <"<<e<<" , "<<n<<">"<<endl;
+    cout <<"Clave privado : <"<<d<<" , "<<n<<">"<<endl;
+}
+
+RSA::RSA(ZZ clave1, ZZ clave2)
+{
+    e = clave1;
+    n = clave2;
+}
+vector<ZZ> RSA::pre(ZZ d){
+    vector<ZZ> auxiliar;
+    ZZ d1 = modulo(d,p-1);
+    ZZ d2 = modulo(d,q-1);
+    auxiliar.push_back(d1);
+    auxiliar.push_back(d2);
+    return auxiliar;
+}
+ZZ RSA::Resto_Chino(vector<ZZ> g,ZZ num){
+    ZZ D1 = modulo(num,p);
+    D1 = potencia(D1,g[0],p);
+    ZZ D2 = modulo(num,q);
+    D2 = potencia(D2,g[1],q);
+    ZZ P = p*q;
+    ZZ P1 = P/p;
+    ZZ P2 = P/q;
+    ZZ Q1 = inversa(P1,p);
+    ZZ Q2 = inversa(P2,q);
+    ZZ resultado = modulo(D1 * P1 * Q1,P);
+    resultado += modulo(D2 * P2 * Q2,P);
+    resultado = modulo(resultado,P);
+    return resultado;
+}
+string RSA::orden1(int a){
+    string tmp=int_to_string(a);
+    string tmp_1=int_to_string(alfabeto.size());
+    while(tmp.size()<tmp_1.size()){
+        tmp='0'+tmp;
+    }
+    return tmp;
+}
+string RSA::orden2(ZZ a,ZZ n){
+    string tmp=zzToString(a);
+    string tmp_1=zzToString(n);
+    while(tmp.size()<tmp_1.size()){
+        tmp='0'+tmp;
+    }
+    return tmp;
+}
+string RSA::orden3(ZZ a,ZZ n){
+    string tmp=zzToString(a);
+    string tmp_1=zzToString(n);
+    while(tmp.size()<(tmp_1.size()-1)){
+        tmp='0'+tmp;
+    }
+    return tmp;
+}
+string RSA::cifrar(string mensaje)
+{
+    cout<<"Cifrado..."<<endl;
+    string result;
+    string tmp_1;
+    string tmp_2;
+    string tmp_3;
+    vector <string> aux_1;
+    vector <ZZ> aux_2;
+    ZZ auxiliar;
+    tmp_2=zzToString(n);
+    for(int i=0;i<mensaje.size();i++){
+        tmp_1+=orden1(alfabeto.find(mensaje[i]));
+    }
+    while(modulo(ZZ(tmp_1.size()),ZZ(tmp_2.size()-1))!=0){
+        tmp_1+=zzToString(ZZ(alfabeto.find('W')));
+    }
+    int bloques=tmp_2.size()-1;
+    for(int i=0;i<tmp_1.size();i++){
+        tmp_3+=tmp_1[i];
+        if(tmp_3.size()==bloques){
+            aux_1.push_back(tmp_3);
+            tmp_3="";
+        }
+    }
+    cout<<e<<endl;
+    for(unsigned int i=0;i<aux_1.size();i++){
+        auxiliar=potencia(stringTozz(aux_1[i]),e,n);
+        result+=orden2(auxiliar,n);
+    }
+    cout<<result<<endl;
+    return result;
+}
+//string RSA::descifrar(string a)
+//{
+//    string result;
+//    string tmp_1=zzToString(n);
+//    string tmp_2;
+//    string tmp_3;
+//    string aux_2;
+//    string aux_3;
+//    ZZ auxiliar;
+//    vector <string> aux_1;
+//    int bloques=tmp_1.size();
+//    for(int i=0;i<a.size();i++){
+//        tmp_2+=a[i];
+//        if(tmp_2.size()==bloques){
+//            aux_1.push_back(tmp_2);
+//            tmp_2="";
+//        }
+//    }
+//    for(int i=0;i<aux_1.size();i++){
+//       auxiliar=potencia(zzToString(aux_1[i]),d,n);
+//       aux_2+=orden3(auxiliar,n);
+//    }
+//    for(int i=0;i<aux_2.size();i++){
+//        aux_3+=aux_2[i];
+//        string alf=int_to_string(alfabeto.size());
+//        if(aux_3.size()==alf.size()){
+//            cout<<aux_3<<" ";
+//            result+=alfabeto[string_to_int(aux_3)];
+//            aux_3="";
+//        }
+//    }
+//    return result;
+//}
+
+
+
